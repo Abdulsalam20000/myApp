af269907ffd5a4181a398bcb913c93a2795109d2
Abdulsalam20000 <20193925@stu.uob.edu.bh>
2023-12-05 19:38:20 +0300

    fixed error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDeNLPZSo1CXuqJQ9c70SuiZ_IIy6fJvHQ",
+    authDomain: "bioattend-9b14b.firebaseapp.com",
+    databaseURL: "https://bioattend-9b14b-default-rtdb.firebaseio.com",
+    projectId: "bioattend-9b14b",
+    storageBucket: "bioattend-9b14b.appspot.com",
+    messagingSenderId: "1080357571656",
+    appId: "1:1080357571656:web:f773c3dc94faf1f52a5c7d",
+    measurementId: "G-0NEPEJSVLM"
+  }
 };
 
 /*
