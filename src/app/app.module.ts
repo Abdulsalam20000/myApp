dc3a7ace5fd2b4277c97d3e0293ab6e03af86229
Abdulsalam20000 <20193925@stu.uob.edu.bh>
2023-12-05 20:13:14 +0300

    firebase integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,25 +10,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDeNLPZSo1CXuqJQ9c70SuiZ_IIy6fJvHQ",
-  authDomain: "bioattend-9b14b.firebaseapp.com",
-  databaseURL: "https://bioattend-9b14b-default-rtdb.firebaseio.com",
-  projectId: "bioattend-9b14b",
-  storageBucket: "bioattend-9b14b.appspot.com",
-  messagingSenderId: "1080357571656",
-  appId: "1:1080357571656:web:f773c3dc94faf1f52a5c7d",
-  measurementId: "G-0NEPEJSVLM"
+ apiKey: "AIzaSyDeNLPZSo1CXuqJQ9c70SuiZ_IIy6fJvHQ",
+ authDomain: "bioattend-9b14b.firebaseapp.com",
+ projectId: "bioattend-9b14b",
+ storageBucket: "bioattend-9b14b.appspot.com",
+ messagingSenderId: "1080357571656",
+ appId: "1:1080357571656:web:f773c3dc94faf1f52a5c7d",
+ measurementId: "G-0NEPEJSVLM"
 };
 
-
-
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+ declarations: [AppComponent],
+ imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+ providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: 'firebase.app',
+      useFactory: () => {
+        return provideFirebaseApp(() => initializeApp(firebaseConfig));
+      },
+      multi: true,
+    },
+    {
+      provide: 'firebase.firestore',
+      useFactory: () => {
+        return provideFirestore(() => getFirestore());
+      },
+      multi: true,
+    },
+ ],
+ bootstrap: [AppComponent],
 })
 export class AppModule {}
